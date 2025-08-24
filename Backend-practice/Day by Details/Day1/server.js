e44c981478c0a5b11579d4e83575b325ffa3ec80
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-08-24 20:51:09 +0530

    Directory changed

--- Backend-practice/Day by Details/Day1/server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-
-let app = http.createServer((req, res) => {
-  res.end('Hello world form the server!');
-});
-
-app.listen('3000', () => {
-  console.log('Server is running on port 3000');
-});
