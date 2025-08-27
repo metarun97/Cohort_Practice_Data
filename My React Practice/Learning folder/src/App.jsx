07b1eebaf5f0cef2f14a1b2792c2bcc5704fc525
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-08-28 00:33:45 +0530

    React Day 3 Done☑️

--- My React Practice/Learning folder/src/App.jsx
+++ My React Practice/Learning folder/src/App.jsx
@@ -1,6 +1,19 @@
 const App = () => {
-  return <div>App</div>;
+  const handleBtnClick = () => {
+    alert('Hello world');
+  };
+
+  const handleMsgData = (msg) => {
+    alert(msg);
+  };
+
+  return (
+    <>
+      <div>App</div>
+      <button onClick={handleBtnClick}>Click</button>
+      <button onClick={handleMsgData('React UI is better then HTML,CSS & JS UI')}>MSG Button</button>
+    </>
+  );
 };
 
 export default App;
-
