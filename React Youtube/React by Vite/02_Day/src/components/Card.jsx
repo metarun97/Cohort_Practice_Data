f57315ba9eb3ec89bbfdaca53961e20e3224cec8
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-10-27 14:37:35 +0530

    Day 2 React done☑️

---
+++ React Youtube/React by Vite/02_Day/src/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Tarun</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+        suscipit.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ React Youtube/React by Vite/02_Day/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <h1>My Site</h1>
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
