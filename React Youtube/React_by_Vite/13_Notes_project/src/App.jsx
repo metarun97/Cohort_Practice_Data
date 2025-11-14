4c75b38104d760375233132aff6ef42e685d4d97
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-11-14 23:26:47 +0530

    Notes app revision done☑️

--- React Youtube/React_by_Vite/13_Notes_project/src/App.jsx
+++ React Youtube/React_by_Vite/13_Notes_project/src/App.jsx
@@ -1,82 +1,81 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
+import { RxCross2 } from 'react-icons/rx';
 
 const App = () => {
   const [title, setTitle] = useState('');
-  const [detail, setDetail] = useState('');
-  const [task, setTask] = useState([]);
+  const [details, setDetails] = useState('');
+  const [task, settask] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const newTask = [...task];
-    newTask.push({ title, detail });
-    setTask(newTask);
-
-    console.log(newTask);
-
+    const copyTask = [...task];
+    copyTask.push({ title, details });
+    settask(copyTask);
+    console.log(copyTask);
     setTitle('');
-    setDetail('');
+    setDetails('');
   };
 
-  const deleteNotes = () => {
-    console.log('Note deleted');
+  const deleteTask = (idx) => {
+    const copyTask = [...task];
+    copyTask.splice(idx, 1);
+    settask(copyTask);
   };
 
   return (
-    <div className="w-full h-screen flex flex-col gap-5  lg:flex lg:flex-row bg-[#333]">
-      <div className="w-full lg:w-1/2 pt-20">
-        <h1 className="text-center text-6xl text-white text-bold">Add Notes</h1>
-        <form
-          action=""
-          onSubmit={(e) => handleSubmit(e)}
-          className="w-full p-8 flex gap-y-6 flex-col items-start text-white"
-        >
-          <input
-            className="p-4 w-full border-0 outline-1 outline-[#ccc] rounded-md"
-            type="text"
-            placeholder="Enter note heading..."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            className="px-4 pb-10 pt-2 w-full border-0 outline-1 outline-[#ccc] rounded-md"
-            placeholder="Enter notes..."
-            value={detail}
-            onChange={(e) => setDetail(e.target.value)}
-          ></textarea>
-          <button className="p-4 text-xl w-full bg-red-500 hover:bg-red-600 transition duration-300 ease-in-out active:scale-95 font-bold border-0  rounded-md cursor-pointer">
-            Add Notes
-          </button>
-        </form>
-      </div>
-      <div className="lg:w-1/2 lg:border-l-2 border-white pt-20">
-        <div className="w-full flex flex-col gap-10 items-center ">
-          <h1 className="text-5xl lg:text-6xl text-white text-bold text-center">
-            Your Notes...⬇️
-          </h1>
-          <div className="w-full h-full flex-wrap Notes flex lg:flex-wrap gap-4 p-4 overflow-auto">
-            {task.map((elem, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className="note relative flex flex-col items-center justify-between w-65 h-80 bg-cover bg-[url('https://static.vecteezy.com/system/resources/previews/010/793/873/non_2x/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')] p-7 rounded-2xl lg:w-52 lg:h-65 "
+    <div className="h-screen lg:flex lg:gap-10 bg-[#222] text-white">
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        action=""
+        className="w-full flex flex-col gap-5 lg:w-1/2 p-10"
+      >
+        <h1 className="text-4xl font-bold mb-5">Notes App</h1>
+        <input
+          className="px-5 py-3 rounded border-2 border-amber-50"
+          type="text"
+          value={title}
+          placeholder="Enter task..."
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <textarea
+          className="px-5 py-3 rounded border-2 border-amber-50"
+          id=""
+          placeholder="Enter task details..."
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
+        ></textarea>
+        <button className="px-5 py-3 bg-blue-500 rounded hover:bg-blue-600 transition duration-100 ease-in-out cursor-pointer">
+          Add Notes
+        </button>
+      </form>
+      <div className="lg:w-1/2 p-10 lg:border-l-2">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+          {task.map(function (elem, idx) {
+            return (
+              <div
+                key={idx}
+                className="bg-white h-52 w-40 rounded-2xl p-4 relative"
+              >
+                <button
+                  onClick={() => deleteTask(idx)}
+                  className="absolute right-2 top-4 bg-red-500 p-1 rounded-full cursor-pointer font-bold"
                 >
-                  <div className="flex flex-col justify-items-start text-start">
-                    <h2 className="text-xl text-bold text-red-800">
-                      {elem.title}
-                    </h2>
-                    <p className="text-xs text-[#333]">{elem.detail}</p>
-                  </div>
-                  <button
-                    onClick={deleteNotes}
-                    className="py-.5 px-1.75 bg-red-500 text-white rounded-md cursor-pointer"
-                  >
-                    Delete Note
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+                  <RxCross2 />
+                </button>
+                <h3 className="text-xl font-bold text-gray-900 leading-tight">
+                  {elem.title}
+                </h3>
+                <p className="text-md text-gray-500 mt-1 leading-tight">
+                  {elem.details}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
