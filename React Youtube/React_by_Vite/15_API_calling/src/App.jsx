606bddc154cefaa412202260424a8346f3f806db
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-11-15 11:04:42 +0530

    API Calling revised now✅

--- React Youtube/React_by_Vite/15_API_calling/src/App.jsx
+++ React Youtube/React_by_Vite/15_API_calling/src/App.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const App = () => {
-  const getData = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/');
-    console.log(response.data);
-  };
+  async function getData() {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos/'
+    );
+    console.log(data);
+  }
   return (
     <div>
       <button onClick={getData}>GetData</button>
