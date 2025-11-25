44331b522503bae31e08347328305c1fff849f4f
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-11-25 23:18:46 +0530

    Variables and declaration done✅

---
+++ JS_by_Sheriyans/Video_1/script.js
@@ -0,0 +1,68 @@
+//!__________________________________________________________VIDEO I_________________________________________________________________________
+
+//^ ____WORD VS KEYWORD____:-
+
+//* WORD:-
+// -> JS me vo cheej jo koi matlab na deti ho jissey koi operational work na ho sake vo word hai.
+// Eg-> "lolo","Tarun","Javascript" etc.
+
+//* KEYWORD:-
+// -> JS me vo cheej jo koi matlab rakhti ho jissey koi operational work ho sake vo keyowrd hai.
+// Eg-> let, var , const , for , if etc.
+
+
+//^ ___VARIABLES___:-
+
+// var a;                      //* declaration
+// var a = 10;                 //* declaration & initialization
+
+
+//& var keyword:-
+
+//* Ye apne apko window me add krta hai.
+
+// var b = 25;
+
+//* Ye function scopped hota h.
+
+// function myFun() {
+//   if (true) {
+//     var a = 10;
+//   }
+// }
+
+// Note -> Ye esa h ki jab hum var se declare krenge to vo function me poore me excess ho skta hai yaha block me chale jane means if else condition me andar hone me bhi ye poore funtion me kahi bhi excess kia ja skta hai.
+
+//* Iseey dobra declare kr skta h same name se error nahi ayega kesa bhi.
+
+// var a = 20;
+// var a = 30;
+
+
+//^ Tamporal-dead-zone:-
+
+//& let & const keyword:-
+
+// console.log(a);                         // a isn't defined
+
+//& var keyword:-
+
+// console.log(a);                         // undefined
+// var a = 20;
+//* Kuch is tarha tutta hai ye:-
+// var a;
+// a= 20;
+
+//* Note -> Ye hoisting ki vjha se hota hai jo var ko hi support krta hai isme variable declaration top pe jump krke chala jata hai or vo usey undefined bata deta h.
+
+
+//^ Hoisting per type:-
+
+// Note -> Hoisting ke concept me hamara variable kuch is tarha tutta hai jisme uska declaration part top pe shift ho jata hai or initialized part neeche reh jata hai isi vhjha se undefined milta hai bina kisi error k.
+
+
+// console.log(a);                        // undefined
+
+// var a = undefined;
+// a = 20;
+
