764efeb764c9ed024333952a35314435a1c7caba
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-11-28 22:45:25 +0530

    Operator and controal flow intro done✅

--- JS_by_Sheriyans/Video_1/script.js
+++ JS_by_Sheriyans/Video_1/script.js
@@ -107,4 +107,129 @@
 // falsy Values --> 0,false,"",null,undefined,document.all,NaN.
 // truthy Values --> falsy k alava sb truthy.
 
+//? Operators in JS:-
 
+//^ Arirthmatic operators:-
+
+// console.log(5 + 5);                     // add -> 10
+// console.log("Tar" + "un");              // concat -> Tarun
+// console.log(10 - 5);                    // substract -> 5
+// console.log(2 * 4);                     // multiply -> 8
+// console.log(12 / 6);                    // devide -> 2
+// console.log(15 % 2);                    // modulous -> 1
+// console.log(2 ** 3);                    // exponentiation -> 8
+
+//^ Comparison operators:-
+
+// console.log(20 == 20);                  // true
+// console.log(20 == 18);                  // false
+// console.log(20 == "20");                // true
+// console.log(20 === "20");               // false
+// console.log(20 != 21);                  // true
+// console.log(20 != "20");                // false
+// console.log(20 !== "20");               // true
+// console.log(25 > 20);                   // true
+// console.log(25 < 20);                   // false
+// console.log(20 <= 20);                  // true
+// console.log(32 >= 32);                  // true
+
+//^ Assignment operators:-
+
+// let a = 8;
+// console.log(a+=4);                      // 12
+// console.log(a-=2);                      // 10
+// console.log(a*=2);                      // 20
+// console.log(a/=2);                      // 10
+// console.log(a%=3);                      // 1
+
+//^ Logical operators:-
+
+// console.log(13>12 && 10>12);            // fasle
+// console.log(13>12 && 10<12);            // true
+// console.log(13>12 || 10>12);            // true
+// console.log(13>12 || 14>12);            // true
+// console.log(10>12 || 11>12);            // false
+// console.log(!true);                     // false
+// console.log(!false);                    // true
+
+//~ Note -> Agar hume kisi bhi cheej ka adjact nature check krna h truthy h ya falsy hai to vo hum ussey phle do !! mark lgakr check kr skte hai.
+
+//^ Urenary operators:-
+
+// console.log(+"5");                       // 5
+// console.log(-5);                         // -5
+// console.log(!15)                         // false
+// console.log(typeof 15);                  // number
+// console.log(typeof "hello");             // string
+// console.log(typeof true);                // boolean
+// let a = 5
+// console.log(a++);                        // 5
+// console.log(a);                          // 6
+// console.log(++a);                        // 7
+// console.log(a);                          // 7
+
+//^ Ternary operators:-
+
+// condition ? "true ka code" : "false ka code"
+
+//~ Note -> Agar condtion true hui to ? k baad ka code chalega or agar false hui to : k baad ka code chalega.
+
+// 12 > 13 ? console.log("true") : console.log("false");
+
+//^ typeof in JS:-
+
+// console.log(typeof "tarun");             // string
+// console.log(typeof 12);                  // number
+// console.log(typeof true);                // boolean
+
+//^ instanceof in JS:-
+
+// let a = [];
+// console.log(a instanceof Array);         // true
+
+// let b = {};
+
+// console.log(b instanceof Object);        // true
+
+//~ Note -> Instanceof reference value k liye hi use kiya jaata hai.
+
+
+//? Control flow in JS:-
+
+//^ If else statement:-
+
+// if (12 > 15) {
+//   console.log("correct");
+// } else {
+//   console.log("wrong")
+// }
+
+//~ Note -> If k condition ki statement sahi hogi to if ka bloack chalega nahi to else wala chal jayega.
+
+//^ Switch case:-
+
+// switch (3) {
+//   case 1:
+//     console.log("case 1");
+//     break;
+//   case 2:
+//     console.log("case 2");
+//     break;
+//   case 3:
+//     console.log("case 3");
+//     break;
+//   default:
+//     console.log("har jagha chalunga me to!");
+//     break;
+// }
+
+//^ Early return statement:-
+
+// function myVal(val) {
+//   if (val < 25) return "D"
+//   else if (val < 50) return "C"
+//   else if (val < 75) return "B"
+//   else return "A"
+// }
+
+// console.log(myVal(55));
