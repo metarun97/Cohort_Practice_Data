d9249a1deba72eee80528358e5d978fff0ab6c3b
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-12-01 23:21:45 +0530

    Functions done✅

--- JS_by_Sheriyans/Video_1/script.js
+++ JS_by_Sheriyans/Video_1/script.js
@@ -12,12 +12,10 @@
 // -> JS me vo cheej jo koi matlab rakhti ho jissey koi operational work ho sake vo keyowrd hai.
 // Eg-> let, var , const , for , if etc.
 
-
 //^ ___VARIABLES___:-
 
 // var a;                      //* declaration
 // var a = 10;                 //* declaration & initialization
-
 
 //& var keyword:-
 
@@ -40,7 +38,6 @@
 // var a = 20;
 // var a = 30;
 
-
 //^ Tamporal-dead-zone:-
 
 //& let & const keyword:-
@@ -57,11 +54,9 @@
 
 //* Note -> Ye hoisting ki vjha se hota hai jo var ko hi support krta hai isme variable declaration top pe jump krke chala jata hai or vo usey undefined bata deta h.
 
-
 //^ Hoisting per type:-
 
 // Note -> Hoisting ke concept me hamara variable kuch is tarha tutta hai jisme uska declaration part top pe shift ho jata hai or initialized part neeche reh jata hai isi vhjha se undefined milta hai bina kisi error k.
-
 
 // console.log(a);                        // undefined
 
@@ -76,7 +71,6 @@
 // let b = a;
 // console.log(b + 1);                    // 11
 // console.log(a);                        // 10
-
 
 //^ Reference types-> Arrays, Objects, functions
 
@@ -193,7 +187,6 @@
 
 //~ Note -> Instanceof reference value k liye hi use kiya jaata hai.
 
-
 //? Control flow in JS:-
 
 //^ If else statement:-
@@ -248,7 +241,6 @@
 // }
 // console.log(getVal(84));
 
-
 //* Some questions:-
 
 //* Q1-> Make a grade system for the students so catagrized by his/her scores...?
@@ -267,7 +259,6 @@
 
 // console.log(getGrade(91))
 
-
 //* Q2-> Make a programme of rock,paper & scissor game...?
 
 // function rps(user, comp) {
@@ -282,7 +273,6 @@
 
 //? Loops in JS:-
 
-
 //^ For loop:-
 
 //* Pattern:-
@@ -296,7 +286,6 @@
 // for (let i = 1; i < 101; i++) {
 //   console.log(i);
 // }
-
 
 //^ While loop:-
 
@@ -343,7 +332,6 @@
 //   if (i === 4) break;           // 4 pe rukega
 // }
 
-
 //^ continue statement:-
 
 // for (let i = 1; i < 10; i++) {
@@ -352,7 +340,6 @@
 // }
 
 //^ Some questions related to loops:-
-
 
 //* Q1- Print number from 1 to 10 by for loop...?
 
@@ -450,14 +437,12 @@
 //   }
 // }
 
-
 //* Q11- Run a loop to 20 and skip multiples of 3...?
 
 // for (let i = 1; i < 21; i++) {
 //   if (i % 3 === 0) continue;
 //   console.log(i);
 // }
-
 
 //* Q12- Run a loop to 20 and get 5 odd numbers from the start...?
 
@@ -489,7 +474,7 @@
 
 //~ Note-> Jab hume koi code turant na chalakr jab chalana hai jab hum chahe to vo hum ek function ki madad se kr skte hai jab banakr hum usey call krte hai.
 
-//^ Named function:-
+//^ Declaration/Statement function:-
 
 // function fun() {
 //   console.log("hello world");
@@ -516,16 +501,194 @@
 // }
 // func();
 
-
 //^ Parameter and arguments in function:-
 
+//* Eg1:-
+
 // function danceFun(dancer) {
-//   console.log(`${dancer} is dancing.`)
+//   console.log(`${dancer} is dancing.`);
 // }
 
-// danceFun("Michel Jeckson");
-// danceFun("Tiger Shroff");
-// danceFun("Hritik Roshan");
+// danceFun('Michel Jeckson');
+// danceFun('Tiger Shroff');
+// danceFun('Hritik Roshan');
 
+//* Eg2:-
+
+// function sum(v1, v2) {
+//   console.log(v1 + v2);
+// }
+// sum(2, 5);
+// sum(21, 12);
 
 //~ Note => Jo function name k just baad braces me hai vo hai parameters and jo function call krte me braces me hai vo h arguments.
+
+//^ Default parameters in function:-
+
+// function add(num1 = 0, num2 = 0) {
+//   console.log(num1 + num2)
+// }
+// add(5, 6);
+
+//~ Note => hum default me parameters ki values ko pass kr sakte hai.
+
+//^ Rest operator for function:-
+
+//* Eg1:-
+
+// function values(...val) {
+//   console.log(val);
+// }
+// values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);          // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//* Eg2:-
+
+// function values(a, b, c, ...val) {
+//   console.log(a, b, c, val);
+// }
+// values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);           // 1 2 3 [ 4, 5, 6, 7, 8, 9, 10 ]
+
+
+//^ Return in function:-
+
+//* Eg1:-
+
+// function printVal() {
+//   return 5;
+// }
+// let val = printVal();
+// console.log(val);
+
+//* Eg2:-
+
+// function sum(val){
+//   return 5 + val;
+// }
+
+// let sumRes = sum(6);
+// console.log(sumRes);
+
+//^ Pure/Impure funtions in JS:-
+
+//^ Pure function:-
+
+// let num = 10;
+// function myFunc() {
+//   console.log(num);
+// }
+// myFunc();
+
+//~Note-> Isme num ki value ko koi change nai hua to ye function pure hai.
+
+//^Impure funtions in JS:-
+
+// let num = 10;
+// function myFunc() {
+//   console.log(num++);
+// }
+// myFunc();
+
+//~Note-> Isme num ki value ko change ki h increment se to ye function pure hai.
+
+
+//^ Closuers in JS:-
+
+// function getVal() {
+//   let a = 10;
+//   return function () {
+//     console.log(a)
+//   }
+// }
+// getVal()();
+
+//~ Note-> Isme hum parent function me banaya hua variable ki value ko andar child function me excess kr skte hai.
+
+//^ Lexical scoping in JS:-
+
+// function abc() {
+//   let a = 10;
+//   return function def() {
+//     let b = 20;
+//     return function ghi() {
+//       let c = 30;
+//     }
+//   }
+// }
+
+//~ Note-> Isme a ko poore abc function me excess kara ja skta hai,b ko poore def k andar or c ko poore ghi me excess kia jayega.
+
+
+//^ IIFE (Immedietly invoked function expression) in JS:-
+
+// (function abc() {
+//   console.log("hello world");
+// })()
+
+
+//^ Hoisting for functions in JS:-
+
+// myFun();
+
+// function myFun() {
+//   console.log("hello world");
+// }
+
+
+//~ Note -> hoisting functions me support jaha funtion statement/declaration ya named hoga vha krti hai but kuch jagha jese function expression of arraw function ki condition me ye work  nahi krti h.
+
+
+//* Some questions for practice:-
+
+//* Q1- find the total of the marks of the sudents in exam that is given as parameters...?
+
+// function calculateMarksAddition(...marks) {
+//   let total = 0;
+//   marks.forEach(function (val) {
+//     total += val;
+//   })
+//   return total;
+// }
+// let res = calculateMarksAddition(66, 78, 55, 46, 51);
+// console.log(res);
+
+
+//* Q2- fix this question using early return:-
+
+// function calculateAge(age) {
+//   if (age > 18) return "Allow to vote";
+//   return "Don't allow to vote"
+// }
+// console.log(calculateAge(18));
+
+//* Q3- Pass a function inside a function and exicute it:-
+
+// function myFunc(val) {
+//   val();
+// }
+// myFunc(function () {
+//   console.log("Hey")
+// })
+
+//* Q4- Pridict the output:-
+
+// function outer() {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     console.log(count);
+//   }
+// }
+
+// let counter = outer();
+// counter();
+// counter();
+
+
+//* Q5- Make this function IIFE:-
+
+// (function myFunc(){
+//   console.log("hello world")
+// })();
+
+
+
