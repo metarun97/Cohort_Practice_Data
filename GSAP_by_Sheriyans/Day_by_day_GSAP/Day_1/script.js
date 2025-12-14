41ec18778831fa7b8f1673289f7a2b9ade786482
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-12-14 23:03:01 +0530

    Till Timeline all done✅

---
+++ GSAP_by_Sheriyans/Day_by_day_GSAP/Day_1/script.js
@@ -0,0 +1,65 @@
+// gsap.to("#box1", {
+//   x: 1200,
+//   delay: 2,
+//   duration: 1,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   borderRadius: "50%",
+//   scale: 0.5
+// })
+
+// gsap.to("#box2", {
+//   x: 500,
+//   y: 500,
+//   delay: 2,
+//   duration: 1,
+
+// })
+
+//--------------------------------------------------
+// * Stagger:-
+
+// gsap.from("h1", {
+//   duration: 0.5,
+//   opacity: 0,
+//   y: 20,
+//   stagger: 0.2
+// })
+
+//--------------------------------------------------
+// * Yoyo:-
+
+// gsap.to("#box", {
+//   x: 1200,
+//   delay: 1,
+//   duration: 1,
+//   rotate: 360,
+//   repeat: -1,
+//   yoyo: true,
+// })
+
+//--------------------------------------------------
+//* Timeline:-
+
+// let tl = gsap.timeline();
+
+
+// tl.to("#box1", {
+//   x: 1200,
+//   duration: 1.5,
+//   delay: 1,
+// })
+
+// tl.to("#box2", {
+//   x: 1200,
+//   duration: 1.5,
+// })
+
+// tl.to("#box3", {
+//   x: 1200,
+//   duration: 1.5,
+//   rotate: 360,
+//   scale: 0.5,
+//   borderRadius: "50%",
+// })
+
