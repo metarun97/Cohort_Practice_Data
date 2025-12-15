250b235a24904387aaf70f2c756077700326e49f
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-12-15 18:58:35 +0530

    ScrollTrigger,scroller,trigger,scrub,pin all done✅

---
+++ GSAP_by_Sheriyans/Day_by_day_GSAP/Day_2/script.js
@@ -0,0 +1,37 @@
+// gsap.from("#page2 h1", {
+//   opacity: 0,
+//   x: 500,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: "#page2 h1",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 50%"
+//   }
+// })
+// gsap.from("#page2 h2", {
+//   opacity: 0,
+//   x: -500,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: "#page2 h2",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 50%"
+//   }
+// })
+
+gsap.from("#page2 #box", {
+  scale: 0,
+  duration: 1,
+  rotate: 720,
+  scrollTrigger: {
+    trigger: "#page2 #box",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 30%",
+    markers: true,
+    scrub: 2.5,
+    pin: true
+  }
+})
