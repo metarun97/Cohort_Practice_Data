75d5351b6190abdc686e7ca911510414489dcd18
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-12-15 23:08:30 +0530

    Till SVG Project done✅

--- GSAP_by_Sheriyans/Day_by_day_GSAP/Day_2/script.js
+++ GSAP_by_Sheriyans/Day_by_day_GSAP/Day_2/script.js
@@ -21,17 +21,18 @@
 //   }
 // })
 
-gsap.from("#page2 #box", {
-  scale: 0,
-  duration: 1,
-  rotate: 720,
-  scrollTrigger: {
-    trigger: "#page2 #box",
-    scroller: "body",
-    start: "top 60%",
-    end: "top 30%",
-    markers: true,
-    scrub: 2.5,
-    pin: true
-  }
-})
+// gsap.from("#page2 #box", {
+//   scale: 0,
+//   duration: 1,
+//   rotate: 720,
+//   scrollTrigger: {
+//     trigger: "#page2 #box",
+//     scroller: "body",
+//     start: "top 60%",
+//     end: "top 30%",
+//     markers: true,
+//     scrub: 2.5,
+//     pin: true
+//   }
+// })
+
