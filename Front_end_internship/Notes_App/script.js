03e3ce078928a37ee417cc25307846a26cc48330
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-12-25 23:15:33 +0530

    Crad data bana diya h abhi logic nai bana h✅

---
+++ Front_end_internship/Notes_App/script.js
@@ -0,0 +1,33 @@
+// let noteCard = document.createElement("div");
+//     noteCard.classList.add("note-card");
+
+//     let dataSec = document.createElement("div");
+//     dataSec.classList.add("data-sec");
+
+//     let noteHeading = document.createElement("h4");
+//     noteHeading.textContent = singlenote.noteHead;
+
+//     let noteDetail = document.createElement("p");
+//     noteDetail.textContent = singlenote.noteDet;
+
+//     let btnSec = document.createElement("div");
+//     btnSec.classList.add("btns")
+
+//     let deleteBtn = document.createElement("button");
+//     deleteBtn.classList.add("deleteBtn");
+//     deleteBtn.textContent = "Delete";
+
+//     let editBtn = document.createElement("button");
+//     editBtn.classList.add("editBtn");
+//     editBtn.textContent = "Edit";
+
+//     dataSec.append(noteHeading);
+//     dataSec.append(noteDetail);
+
+//     btnSec.append(deleteBtn);
+//     btnSec.append(editBtn);
+
+//     noteCard.append(dataSec);
+//     noteCard.append(btnSec);
+
+//     notes.append(noteCard);
