71096f9cafe43a68614266c96e59ad796885847c
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-01-03 23:21:07 +0530

    Hero section done✅

---
+++ Front_end_internship/React_Projects/E-commerce/src/components/routes/MainRoutes.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home";
+import Products from "../../pages/Products";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
