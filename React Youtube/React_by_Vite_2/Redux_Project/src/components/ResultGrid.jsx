2f57a4b02891b88c510a73adf8d6c95b23ac1466
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-01-23 21:53:44 +0530

    Commeit for the day

--- React Youtube/React_by_Vite_2/Redux_Project/src/components/ResultGrid.jsx
+++ React Youtube/React_by_Vite_2/Redux_Project/src/components/ResultGrid.jsx
@@ -1,44 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { searchPhotos, searchVideos, searchGifs } from '../api/mediaApi';
-import {
-  setQuery,
-  setActiveTabs,
-  setResults,
-  setLoading,
-  setError,
-  clearResult,
-} from '../redux/features/searchSlice';
-import { useEffect } from 'react';
+import React from 'react';
 
 const ResultGrid = () => {
-  const { query, activeTab, results, loading, error } = useSelector(
-    (store) => store.search,
-  );
-
-  useEffect(
-    function () {
-      let data;
-      const getData = async () => {
-        if (activeTab == 'photos') {
-          let res = await searchPhotos(query);
-          data = res.results;
-        }
-        if (activeTab == 'videos') {
-          let res = await searchVideos(query);
-          data = res.videos;
-        }
-        if (activeTab == 'gif') {
-          let res = await searchGifs(query);
-          data = res.data;
-        }
-        console.log(data);
-      };
-      getData();
-    },
-    [query, activeTab],
-  );
-
-  return <div></div>;
+  return <div>ResultGrid</div>;
 };
 
 export default ResultGrid;
