1aff9c79db60d348285d211e48de187e0c4ad4a1
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-01-25 23:54:20 +0530

    Day commit

--- React Youtube/React_by_Vite_2/Redux_Project/src/api/mediaApi.js
+++ React Youtube/React_by_Vite_2/Redux_Project/src/api/mediaApi.js
@@ -5,7 +5,7 @@ const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
 const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY;
 
 // Fetch image files:-
-export async function searchPhotos(query, page = 1, per_page = 20) {
+export async function fetchPhotos(query, page = 1, per_page = 20) {
   const res = await axios.get('https://api.unsplash.com/search/photos', {
     params: { query, page, per_page },
     headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` }
@@ -14,7 +14,7 @@ export async function searchPhotos(query, page = 1, per_page = 20) {
 }
 
 // Fetch video files:-
-export async function searchVideos(query, per_page = 15) {
+export async function fetchVideos(query, per_page = 15) {
   const res = await axios.get('https://api.pexels.com/videos/search', {
     params: { query, per_page },
     headers: { Authorization: PEXELS_KEY }
@@ -23,7 +23,7 @@ export async function searchVideos(query, per_page = 15) {
 }
 
 // Fetch GIF Files:-
-export async function searchGifs(query, limit = 20) {
+export async function fetchGifs(query, limit = 20) {
   const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
     params: { q: query, limit, api_key: GIPHY_KEY },
   });
--- React Youtube/React_by_Vite_2/Redux_Project/src/components/ResultGrid.jsx
+++ React Youtube/React_by_Vite_2/Redux_Project/src/components/ResultGrid.jsx
@@ -1,4 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPhotos, fetchVideos, fetchGifs } from '../api/mediaApi';
+import {
+  setQuery,
+  setLoading,
+  setError,
+  setResults,
+} from '../redux/features/searchSlice';
+import { useEffect } from 'react';
+
 const ResultGrid = () => {
+  const dispatch = useDispatch();
+
+  const { query, results, activeTab, loading, error } = useSelector(
+    (store) => store.search,
+  );
+
+  useEffect(
+    function () {
+      let data;
+      const getData = async () => {
+        if (activeTab == 'photos') {
+          let response = await fetchPhotos(query);
+          data = response.results;
+        }
+        if (activeTab == 'videos') {
+          data = await fetchVideos(query);
+        }
+        if (activeTab == 'gifs') {
+          data = await fetchGifs(query);
+        }
+        // console.log(data);
+      };
+      getData();
+    },
+    [query, activeTab],
+  );
+
   return <div>ResultGrid</div>;
 };
 
