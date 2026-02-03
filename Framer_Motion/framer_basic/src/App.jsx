4a43294caae4ea9a63d63aea1f1f73bf79cc0ba5
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-02-03 23:31:32 +0530

    Framer motion start✅

---
+++ Framer_Motion/framer_basic/src/App.jsx
@@ -0,0 +1,21 @@
+import { motion } from 'motion/react';
+
+const App = () => {
+  return (
+    <div>
+      <motion.div
+        animate={{
+          x: 1000,
+          rotate:360
+        }}
+        transition={{
+          delay: 2,
+          duration: 3,
+        }}
+        className="box"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default App;
