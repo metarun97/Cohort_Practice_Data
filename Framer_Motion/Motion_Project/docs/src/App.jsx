669d6be99e70a418bee51e436febc3ba82faa935
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-02-04 22:32:42 +0530

    Docs background done✅

---
+++ Framer_Motion/Motion_Project/docs/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="relative w-full h-screen bg-zinc-800">
+      <div className="w-full py-10 absolute top-[5%] flex justify-center font-semibold text-xl text-zinc-600">Documnets.</div>
+      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[13vw] leading-none font-semibold tracking-tight text-zinc-900">Docs.</h1>
+    </div>
+  );
+};
+
+export default App;
