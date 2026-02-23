1cdf48eb35534ddeb68f0526c64ecc1bb5d2bcb3
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-02-23 23:22:57 +0530

    Commit changes of today✅

--- SCS_React_Projects/frontend/src/pages/user/UserProfile.jsx
+++ SCS_React_Projects/frontend/src/pages/user/UserProfile.jsx
@@ -1,133 +1,105 @@
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import {
+  asyncDeleteUser,
+  asyncUpdateUser,
+} from '../../store/actions/userActions';
 
 const UserProfile = () => {
   const { id } = useParams();
   const {
-    productReducer: { products },
     userReducer: { users },
   } = useSelector((state) => state);
 
-  const product = products?.find((product) => product.id == id);
-  console.log(product, users);
-
   const { register, handleSubmit, reset } = useForm({
     defaultValues: {
-      image: product?.image,
-      title: product?.title,
-      price: product?.price,
-      category: product?.category,
-      description: product?.description,
+      username: users?.username,
+      email: users?.email,
+      password: users?.password,
     },
   });
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const updateProductHandler = (product) => {
-    dispatch(asyncUpdateProduct(id, product));
-    navigate('/products');
+  const updateUserHandler = (user) => {
+    dispatch(asyncUpdateUser(users.id, user));
   };
 
   const deleteHandler = () => {
-    dispatch(asyncDeleteProduct(id));
-    navigate('/products');
+    dispatch(asyncDeleteUser(users.id));
+    navigate('/login');
   };
-  return (
+  return users ? (
     <div className="w-full py-20 flex justify-center items-center px-5">
       <form
-        onSubmit={handleSubmit(updateProductHandler)}
-        className="w-full max-w-5xl bg-white rounded-xl p-5"
+        onSubmit={handleSubmit(updateUserHandler)}
+        className="w-full max-w-3xl bg-white rounded-xl p-5"
       >
         <h1 className="text-center font-semibold text-md md:text-2xl mb-5 text-slate-950">
           Update Product
         </h1>
         <div className="w-full ">
           <label
-            htmlFor="image"
-            className="block text-slate-900 font-semibold mb-2 "
-          >
-            Image
-          </label>
-          <input
-            className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
-            type="url"
-            placeholder="Enter image url..."
-            {...register('image')}
-          />
-        </div>
-        <div className="w-full ">
-          <label
             htmlFor="username"
             className="block text-slate-900 font-semibold mb-2 "
           >
-            Title
+            Username
           </label>
           <input
             className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
             type="text"
-            placeholder="Enter title..."
-            {...register('title')}
+            placeholder="Enter username..."
+            {...register('username')}
+          />
+        </div>
+        <div className="w-full ">
+          <label
+            htmlFor="email"
+            className="block text-slate-900 font-semibold mb-2 "
+          >
+            Email
+          </label>
+          <input
+            className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
+            type="email"
+            placeholder="Enter email..."
+            {...register('email')}
           />
         </div>
 
         <div className=" flex flex-col gap-y-1">
           <div className="w-full ">
             <label
-              htmlFor="price"
+              htmlFor="password"
               className="block text-slate-900 font-semibold mb-2"
             >
-              Price
+              Passoword
             </label>
             <input
               className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
-              type="number"
-              placeholder="Enter price..."
-              {...register('price')}
-            />
-          </div>
-          <div className="w-full ">
-            <label
-              htmlFor="description"
-              className="block text-slate-900 font-semibold mb-2 "
-            >
-              Description
-            </label>
-            <textarea
-              className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
-              placeholder="Enter Description..."
-              {...register('description')}
-            ></textarea>
-          </div>
-          <div className="w-full ">
-            <label
-              htmlFor="category"
-              className="block text-slate-900 font-semibold mb-2"
-            >
-              Category
-            </label>
-            <input
-              className="w-full p-1.5 mb-2 rounded border-none outline-1 outline-gray-300 focus:outline-blue-500 text-slate-800"
-              type="text"
-              placeholder="Enter category..."
-              {...register('category')}
+              type="password"
+              placeholder="Enter password..."
+              {...register('password')}
             />
           </div>
           <div className="flex items-center gap-2">
             <button className="w-fit p-1.5 rounded border-none font-semibold bg-blue-500 hover:bg-blue-600 cursor-pointer mt-2.5">
-              Update Product
+              Update user
             </button>
             <button
               type="button"
               onClick={deleteHandler}
               className="w-fit p-1.5 rounded border-none font-semibold bg-red-500 hover:bg-red-600 cursor-pointer mt-2.5"
             >
-              Delete Product
+              Delete user
             </button>
           </div>
         </div>
       </form>
     </div>
+  ) : (
+    'Loading...'
   );
 };
 
--- SCS_React_Projects/frontend/src/store/actions/userActions.jsx
+++ SCS_React_Projects/frontend/src/store/actions/userActions.jsx
@@ -1,29 +1,29 @@
 import axios from '../../api/axiosconfig';
 import { loadUser, removeUser } from '../reducers/userSlice';
 
-// Currentuser action:-
+// Current user action:-
 export const asyncCurrentUser = () => async (dispatch, getState) => {
   try {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user) dispatch(loadUser(user));
-    else console.log("User not logged in!")
+    else console.log('User not logged in!');
   } catch (error) {
     console.log(error);
   }
 };
 
-// Logout action:-
+// Logout user action:-
 export const asyncLogoutUser = (user) => async (dispatch, getState) => {
   try {
     localStorage.removeItem('user');
-    dispatch(removeUser())
-    console.log("User logged out!")
+    dispatch(removeUser());
+    console.log('User logged out!');
   } catch (error) {
     console.log(error);
   }
 };
 
-// Login action:-
+// Login user action:-
 export const asyncLoginUser = (user) => async (dispatch, getState) => {
   try {
     const { data } = await axios.get(
@@ -35,11 +35,29 @@ export const asyncLoginUser = (user) => async (dispatch, getState) => {
   }
 };
 
-// Register action:-
+// Register user action:-
 export const asyncRegisterUser = (user) => async (dispatch, getState) => {
   try {
     const res = await axios.post('/users', user);
     console.log(res[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+// Update user action:-
+export const asyncUpdateUser = (id, user) => async (dispatch, getState) => {
+  try {
+    const { data } = await axios.patch('/users/' + id, user);
+    localStorage.setItem('user', JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// Delete user action:-
+export const asyncDeleteUser = (id) => async (dispatch, getState) => {
+  try {
+    await axios.delete('/users/' + id);
+    dispatch(asyncLogoutUser());
   } catch (error) {
     console.log(error);
   }
