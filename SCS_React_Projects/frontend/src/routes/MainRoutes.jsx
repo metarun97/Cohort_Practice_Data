a0423411c1a931a049290b15f425701ec0f2d461
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-02-24 23:35:39 +0530

    Commit again✅

--- SCS_React_Projects/frontend/src/routes/MainRoutes.jsx
+++ SCS_React_Projects/frontend/src/routes/MainRoutes.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './../pages/Home';
 import Products from './../pages/Products';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
@@ -13,7 +12,7 @@ const MainRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/" element={users ? <Products /> : <Home />} />
+      <Route path="/" element={<Products />} />
       {/* <Route path="/products" element={<Products />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
