2c54a280329f776f28ed705f36709a5344165e66
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-03-15 00:10:36 +0530

    Image rendering in next js done✅

--- Next-JS/next-app3/app/home/page.js
+++ Next-JS/next-app3/app/home/page.js
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
+import women from '../../public/women.avif'
 
 const page = () => {
   return (
     <div>
       <h3>Home</h3>
+      <Image width={600} height={400} src={women} alt='not-found'/>
     </div>
   )
 }
