a54403d5a34f32c4861f2b10dea2b2a3a32f2ac9
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-03-27 21:08:27 +0530

    register,login,user,logout all done✅

--- Backend-practice/Day_by_day Practice/Day_135_revision/src/routes/auth.routes.js
+++ Backend-practice/Day_by_day Practice/Day_135_revision/src/routes/auth.routes.js
@@ -21,9 +21,10 @@ router.post("/register", async (req, res) => {
     username
   })
 
+  // User vo mila me username krra hu to ye code chalega:-
   if (isUserAlreadyExist) {
     return res.status(409).json({
-      message: "User alredy in use!",
+      message: "Username alredy in use!",
     })
   }
 
@@ -48,6 +49,82 @@ router.post("/register", async (req, res) => {
 })
 
 
+// ^ User API Creation:-
+router.get("/user", async (req, res) => {
+  const token = req.cookies.token;
+
+  // Agar token aya hi nahi tab ye krna hai:-
+  if (!token) {
+    return res.status(401).json({
+      message: "Unautherized Token not found!",
+    })
+  }
+
+  // Agar token aya h tab token ko check krna hai:-
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findOne({
+      _id: decoded.id,
+    })
+
+    res.status(200).json({
+      message: "User fetched successfully!",
+      user,
+    })
+
+  } catch (error) {
+    res.status(401).json({
+      message: "Unautherized Invalid token!",
+    })
+  }
+})
+
+
+//^ Login API Creation:-
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Username se find krenge esa koi username ka user hai ya nahi:-
+  const user = await userModel.findOne({ username })
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found!"
+    })
+  }
+
+  // User sahi lila to passoword check ki sahi hai ya nahi:-
+  const isPasswordValid = user.password === password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password!"
+    })
+  }
+
+  // Token banakr token cookie me dalo:-
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User loggedin successfully!",
+    user,
+  })
+
+
+
+})
+
+
+//^ Login API Creation:-
+router.get("/logout", async (req, res) => {
+  res.clearCookie("token")
+
+  res.status(200).json({
+    message: "User logout successfully!",
+  })
+})
 
 
 // router export kia gaya:-
