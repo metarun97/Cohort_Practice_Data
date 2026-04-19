45a09ef6241d0b2fc39088176230431c4f3cc37f
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-04-19 23:18:42 +0530

    ChatHistory STM done✅

--- Backend-practice/Day_by_day Practice/Chat_GPT_Project/src/socket/socket.server.js
+++ Backend-practice/Day_by_day Practice/Chat_GPT_Project/src/socket/socket.server.js
@@ -7,87 +7,76 @@ const aiService = require("../services/ai.service");
 const messageModel = require("../models/message.model");
 
 
-/* Socket function initiate here:- */
+/* Socket function initiated here:- */
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {/* options */ });
 
-  //* Middleware to check if vaid user here and it has a valid token only  then it can connect to Socket:-
+  // Middleware that check if user logged in then it can connect to socket server:-
   io.use(async (socket, next) => {
     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
-    // If token not found then show error ruther then you check user by id and set user in socket's
+    // If token not found:-
     if (!cookies.token) {
-      next(new Error("Authentication error: Token not found!"));
+      next(new Error("Authentication error: Token not provided!"));
     }
 
+    // Now found the token & check it is comming from valid user then add it inside socket's user:-
     try {
       const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
       const user = await userModel.findById(decoded.id);
       socket.user = user;
       next();
-
-    } catch (err) {
-      next(new Error("Authentication error: Invalid token!"))
+    } catch (error) {
+      next(new Error("Authentication errror: Invalid Token!"))
     }
   })
 
+
+  // Socket connection:-
   io.on("connection", (socket) => {
-    // console.log("New socket user", socket.user);
-    // console.log("New socket connection", socket.id);
+    // console.log("New socket user:", socket.user);
+    // console.log("New socket user's id:", socket.id);
+
 
     socket.on("ai-message", async (messagePayload) => {
-      console.log(messagePayload);  /* {chat,content} */
+      // console.log(messagePayload);
 
-
-
-      // save message when user send a message to AI model:-
       await messageModel.create({
-        chat: messageModel.chat,
+        chat: messagePayload.chat,
         user: socket.user._id,
         content: messagePayload.content,
-        role: "user",
+        role: "user"
       })
 
-      const chatHistory = await messageModel.find({
-        chat: messagePayload.chat
-      })
+      const chatHistory = (await messageModel.find({
+        chat: messagePayload.chat,
+      }).sort({ createdAt: -1 }).limit(20).lean()).reverse();
 
-      // console.log("Chat History:", chatHistory.map((item) => {
-      //   return {
-      //     role: item.role,
-      //     part: [{ text: item.content }]
-      //   }
-      // }));
+      // console.log(chatHistory)
 
-
-      try {
-        const response = await aiService.generateResponse(chatHistory.map((item) => {
+      const response = await aiService.generateResponse(chatHistory.map(item => {
         return {
-          role: item.role,
-          part: [{ text: item.content }]
+          role: "user",
+          parts: [{ text: item.content }],
         }
       }));
 
-        // Save message when AI model send us a message back:-
-        await messageModel.create({
-          chat: messageModel.chat,
-          user: socket.user._id,
-          content: response,
-          role: "model",
-        })
 
-        socket.emit("ai-response", {
-          content: response,
-          chat: messagePayload.chat,
-        })
-      } catch (error) {
-        socket.emit("ai-response", {
-          error: "AI is busy, try again later 🙏"
-        })
-        console.log(error);
-      }
+      await messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: response,
+        role: "model"
+      })
+
+
+      socket.emit("ai-response", {
+        content: response,
+        chat: messagePayload.chat
+      })
+
+
     })
-
   })
 }
 
