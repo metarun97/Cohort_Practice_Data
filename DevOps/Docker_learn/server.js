2eae0787a2641e9af2569fc9a63de70fcac12fca
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-05-18 21:00:07 +0530

    Docker installer and ECR ,ECS introduced✅

---
+++ DevOps/Docker_learn/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "Hello world" })
+})
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
