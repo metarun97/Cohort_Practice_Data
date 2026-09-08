2c48d831db63a29c33ea23c11080d5ef8656834e
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-09-08 16:34:56 +0530

    Video 1 DSA completed.✅

--- DSA_SCS_Youtube/video_one.js
+++ DSA_SCS_Youtube/video_one.js
@@ -62,4 +62,68 @@
 
 //^ Date - 08/09/2026
 
+//* Q1->
 
+// let a = 11,  // 12 => 13
+//   b = 22;    // 23 => 24
+
+//      // 11 + 22 + 11  + 22  +  13 + 24
+// let c = a  + b  + a++ + b++ + ++a + ++b;
+// console.log(a)  // 13
+// console.log(b)  // 24
+// console.log(c)  // 66 + 13 + 24 => 103
+
+
+//* Q2->
+// let i = true;
+
+// i++;
+// console.log(i)  // 2
+
+
+//* Q3->
+// let a = 10++;
+// console.log(a);
+
+//* Q4->
+
+// let a = 10;
+// let b = ++(a++);
+// console.log(b);
+
+//~ Note => Ap kabhi bhi constant value pe pre ya post [increment/decrement] nahi laga sakte bs ap variable pe hi laga sakte ho.
+
+//* Q5-> Find the area of a rectangle?
+
+// let length = 12;
+// let bredth = 6;
+
+// console.log(`Area of rectangle: ${length * bredth}`);           // 72
+
+//* Q6-> Find the perimeter of a rectangle?
+
+// let length = 12;
+// let bredth = 6;
+
+// console.log(`Area of rectangle: ${2 * (length + bredth)}`);     // 36
+
+//* Q7-> Genreate four digit OTP?
+
+// console.log(Math.floor(Math.random() * 9000) + 1000);
+
+
+//* Q8-> Find the area of a triangle by Heron's formule?
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+
+// let s = (a + b + c) / 2;
+
+// console.log(`Area of triange: ${Math.sqrt(s * (s - a) * (s - b) * (s - c))}`);
+
+//* Q9-> Find the circumfrence of a circle?
+
+// let r = 12;
+
+// console.log(Number(2 * Math.PI * r).toFixed(2));
