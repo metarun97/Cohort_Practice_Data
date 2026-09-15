4170ecd4b1b63471e3d01c9627a74cb947273089
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-09-15 18:43:38 +0530

    Three pattern undestood today☑️

---
+++ DSA_SCS_Youtube/video_four.js
@@ -0,0 +1,62 @@
+//! Day 4 questions:-
+//^ Date - 14/09/2026
+
+//* This is a node invironment function to write code in a single line.
+
+// process.stdout.write("hello ");
+// process.stdout.write("world");
+
+
+//* Q1->  Basic pattern for the star printing?
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// const prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter your n value"));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log("")
+// }
+
+
+//* Q2->  Right angle pattern for the star printing?
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter your n value: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(" *");
+//   }
+//   console.log();
+// }
+
+
+//* Q3->  Right angle pattern for the number printing start from 1?
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Enter your n value: "));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(j + " ");
+  }
+  console.log();
+}
