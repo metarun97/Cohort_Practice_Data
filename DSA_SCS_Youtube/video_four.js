636194e4164d32ef69578244769affaffda52c46
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-09-16 20:40:38 +0530

    2 pattern done☑️

--- DSA_SCS_Youtube/video_four.js
+++ DSA_SCS_Youtube/video_four.js
@@ -51,12 +51,51 @@
 // 1 2 3 4
 // 1 2 3 4 5
 
-let prompt = require("prompt-sync")();
-let n = Number(prompt("Enter your n value: "));
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter your n value: "));
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write(j + " ");
-  }
-  console.log();
-}
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(j + " ");
+//   }
+//   console.log();
+// }
+
+//* Q4->  Right angle pattern for alphabets?
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter n value: "));
+
+// for (let i = 1; i <= n; i++) {
+//   let ascaii = 65;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(ascaii) + " ");
+//     ascaii++;
+//   }
+//   console.log();
+// }
+
+//* Q5->  Mirror right angle pattern for the star printing?
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter your n value: "));
+
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+
